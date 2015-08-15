3aea11e4cd9f12884837f12f12b74a9ad01719be
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2015-08-15 00:12:40 -0400

    added tests

--- test/unit/lib/perf/Emailing/EmailTest.php
+++ test/unit/lib/perf/Emailing/EmailTest.php
@@ -19,7 +19,105 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testGetToWithoutTo()
+    public function testGetCharsetDefault()
+    {
+        $result = $this->email->getCharset();
+
+        $this->assertSame('utf-8', $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetCharsetModified()
+    {
+        $charset = 'foo';
+
+        $this->email->setCharset($charset);
+
+        $result = $this->email->getCharset();
+
+        $this->assertSame($charset, $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetFromDefault()
+    {
+        $result = $this->email->getFrom();
+
+        $this->assertNull($result);
+    }
+
+    /**
+     *
+     */
+    public function testGetFromModified()
+    {
+        $from = 'foo@bar.baz';
+
+        $this->email->setFrom($from);
+
+        $result = $this->email->getFrom();
+
+        $this->assertSame($from, $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetFromModifiedToNull()
+    {
+        $this->email->setFrom('foo@bar.baz');
+        $this->email->setFrom(null);
+
+        $result = $this->email->getFrom();
+
+        $this->assertNull($result);
+    }
+
+    /**
+     *
+     */
+    public function testGetReplyToDefault()
+    {
+        $result = $this->email->getReplyTo();
+
+        $this->assertNull($result);
+    }
+
+    /**
+     *
+     */
+    public function testGetReplyToModified()
+    {
+        $replyTo = 'foo@bar.baz';
+
+        $this->email->setReplyTo($replyTo);
+
+        $result = $this->email->getReplyTo();
+
+        $this->assertSame($replyTo, $result);
+    }
+
+    /**
+     *
+     */
+    public function testGetReplyToModifiedToNull()
+    {
+        $this->email->setReplyTo('foo@bar.baz');
+        $this->email->setReplyTo(null);
+
+        $result = $this->email->getReplyTo();
+
+        $this->assertNull($result);
+    }
+
+    /**
+     *
+     */
+    public function testGetToDefault()
     {
         $result = $this->email->getTo();
 
@@ -30,7 +128,7 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testGetCcWithoutCc()
+    public function testGetCcDefault()
     {
         $result = $this->email->getCc();
 
@@ -41,7 +139,7 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testGetBccWithoutBcc()
+    public function testGetBccDefault()
     {
         $result = $this->email->getBcc();
 
@@ -52,7 +150,7 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testGetSubjectWithoutSubject()
+    public function testGetSubjectDefault()
     {
         $result = $this->email->getSubject();
 
@@ -63,7 +161,7 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testGetMessageWithoutMessage()
+    public function testGetMessageDefault()
     {
         $result = $this->email->getMessage();
 
